b2c266bf1ca7152aab646257240fa85585043954
Ubuntu <ubuntu@ip-172-31-3-149.us-east-2.compute.internal>
2021-02-19 20:29:35 +0000

    config dockerfile and docker-compose

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const MainRouter = require('./src/route');
 const app = express();
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('welcome to my world')
+    res.send('HELLO MY FUCKING SHIT')
 });
 
 app
     .use(bodyParser.urlencoded({ extended: true }))
     .use(bodyParser.json());
 
-app.use('/api', MainRouter);
+app.use('/api', MainRouter)
 
-app.listen(3001, () => {
-    console.log(`server running`)
-});
+app.listen(port, host ,() => console.log(`running on port ${port}`))
